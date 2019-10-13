3708d1c45eb3faaa49217796bd25be253978b053
regwise <56517810+regwise@users.noreply.github.com>
2019-10-14 02:54:14 +0800

    Add files via upload

---
+++ js/min.js
@@ -0,0 +1,28 @@
+(function() {
+  var stripe = Stripe('pk_live_DKxmRp3iMVOQ8JcCCujgltje');
+
+  var checkoutButton = document.getElementById('checkout-button-sku_Fz8qqtln839tiu');
+  checkoutButton.addEventListener('click', function () {
+    // When the customer clicks on the button, redirect
+    // them to Checkout.
+    stripe.redirectToCheckout({
+      items: [{sku: 'sku_Fz8qqtln839tiu', quantity: 1}],
+
+      // Do not rely on the redirect to the successUrl for fulfilling
+      // purchases, customers may not always reach the success_url after
+      // a successful payment.
+      // Instead use one of the strategies described in
+      // https://stripe.com/docs/payments/checkout/fulfillment
+      successUrl: 'https://modalert.site/payment-success.html',
+      cancelUrl: 'https://modalert.site/payment-failed.html',
+    })
+    .then(function (result) {
+      if (result.error) {
+        // If `redirectToCheckout` fails due to a browser or network
+        // error, display the localized error message to your customer.
+        var displayError = document.getElementById('error-message');
+        displayError.textContent = result.error.message;
+      }
+    });
+  });
+})();
